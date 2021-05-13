0d5703f59db02a2bf0f85579a1656fc9a24d2439
YinanWang16 <ywang16@illumina.com>
2021-05-13 10:05:00 +1000

    Debug tsv2json.py

--- tsv2json.py
+++ tsv2json.py
@@ -5,7 +5,11 @@
 import pandas as pd
 import numpy as np
 import json
-import sys
+import sys, getopt
+
+def usage():
+    print(sys.argv[0] + ' <tsv_file>')
+    sys.exit(2)
 
 def tsv2json(tsv_file):
     df = pd.read_csv(tsv_file, sep='\t', header=0 )
@@ -17,9 +21,13 @@ def tsv2json(tsv_file):
 def convert(o):
     if isinstance(o, np.generic): return o.item()
     raise TypeError
-    
+
 def main():
-    tsv_file = sys.args[1]
+    if len(sys.argv) == 1:
+        usage()
+    else:
+        tsv_file = sys.argv[1]
+
     json_file = tsv_file.replace('tsv', 'json')
     variant_df = tsv2json(tsv_file)
     with open(json_file, 'w') as jf:
